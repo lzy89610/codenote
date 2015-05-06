9a7f86b01785ec4b73961c35d675177c43458cea
leonstar <254759426@qq.com>
2015-05-06 11:45:11 +0800

    io

--- src/main/java/com/codenote/j2se/file/ReaderDemo.java
+++ src/main/java/com/codenote/j2se/file/ReaderDemo.java
@@ -4,39 +4,60 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.io.PrintWriter;
 
 
 public class ReaderDemo
 {
 
-	public static void main(String[] args) throws IOException
+	public static void main(String[] args)
 	{
 		String to = "tmp/to.txt";
 		String from = "tmp/from.txt";
 		writeFile(from, to);
 	}
 	
-	public static void writeFile(String from, String to) throws IOException
+	public static void writeFile(String from, String to)
 	{
-		FileReader reader = new FileReader(new File(from));
-		BufferedReader br = new BufferedReader(reader);
+		BufferedReader br = null;
+		PrintWriter bw = null;
 		
-		PrintWriter bw = new PrintWriter(new FileWriter(new File(to)));
-		
-		int s;
-		
-		while((s=br.read()) != -1)
+		try
 		{
-			System.out.println(s);
-			bw.write((char)s);
+			br = new BufferedReader(new FileReader(new File(from)));
+			
+			bw = new PrintWriter(new FileWriter(new File(to)));
+			
+			int s;
+			
+			while((s=br.read()) != -1)
+			{
+				System.out.println(s);
+				bw.write((char)s);
+			}
 		}
-		
-		bw.close();
-		
-		
-		
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			try
+			{
+				if(bw != null)
+				{
+					bw.close();
+				}
+				if(br != null)
+				{
+					br.close();
+				}
+			}
+			catch (Exception e2)
+			{
+				e2.printStackTrace();
+			}
+		}
 	}
 
 }
